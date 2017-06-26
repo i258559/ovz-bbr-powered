c4e8c21d6dc3de9f9c803d67c3faf9dddad09057
mishaelre <664513537@qq.com>
2017-06-26 17:06:13 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,101 @@
+#! /bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+#=================================================================#
+#   System Required:  Debian8_x64                                   #
+#   Description: One click Install lkl-bbr kcp               #
+#   Adapt from: 91yun <https://twitter.com/91yun>                     #
+#   Thanks: @linrong                            #
+#=================================================================#
+
+if [[ $EUID -ne 0 ]]; then
+   echo "Error:This script must be run as root!" 1>&2
+   exit 1
+fi
+
+
+Get_Dist_Name()
+{
+    if grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+        release='CentOS'
+    elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+        release='Debian'
+    elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
+        release='Ubuntu'
+	else
+        release='unknow'
+    fi
+    
+}
+Get_Dist_Name
+function getversion(){
+    if [[ -s /etc/redhat-release ]];then
+        grep -oE  "[0-9.]+" /etc/redhat-release
+    else    
+        grep -oE  "[0-9.]+" /etc/issue
+    fi    
+}
+ver=""
+CentOSversion() {
+    if [ "${release}" == "CentOS" ]; then
+        local version="$(getversion)"
+        local main_ver=${version%%.*}
+		ver=$main_ver
+    else
+        ver="$(getversion)"
+    fi
+}
+CentOSversion
+Get_OS_Bit()
+{
+    if [[ `getconf WORD_BIT` = '32' && `getconf LONG_BIT` = '64' ]] ; then
+        bit='x64'
+    else
+        bit='x32'
+    fi
+}
+Get_OS_Bit
+
+if [ "${release}" == "CentOS" ]; then
+	yum install -y bc
+else
+	apt-get update
+	apt-get install -y bc
+fi
+
+iddver=`ldd --version | grep ldd | awk '{print $NF}'`
+dver=$(echo "$iddver < 2.14" | bc)
+if [ $dver -eq 1 ]; then
+	ldd --version
+	echo "idd的版本低于2.14，系统不支持。请尝试Centos7，Debian8，Ubuntu16"
+	exit 1
+fi
+
+if [ "$bit" -ne "x64" ]; then
+	echo "脚本目前只支持64bit系统"
+	exit 1
+fi	
+
+
+
+
+
+if [ "${release}" == "CentOS" ]; then
+	yum install -y haproxy
+elif [[ "${release}" == "Debian" && "$ver" == "7" ]]; then
+	echo "deb http://ftp.debian.org/debian wheezy-backports main" >> /etc/apt/sources.list
+	apt-get install debian-archive-keyring
+	apt-key update
+	apt-get update
+	apt-get install -y haproxy
+	update-rc.d haproxy disable
+else
+	apt-get update
+	apt-get install -y haproxy
+	update-rc.d haproxy disable
+fi
+
 cd /root
-apt-get update
 apt-get install -y git
 git clone -b manyuser https://github.com/shadowsocksr/shadowsocksr.git
 bash /root/shadowsocksr/initcfg.sh
