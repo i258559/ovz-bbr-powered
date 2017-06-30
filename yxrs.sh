cbecd835e59b83a76517dd725ceb9118148b06c4
mishaelre <664513537@qq.com>
2017-06-30 20:09:56 +0800

    Update yxrs.sh

--- yxrs.sh
+++ yxrs.sh
@@ -6,8 +6,76 @@ export PATH
 #   Description: One click Install lkl-bbr           #
 #=================================================================#
 
+if [[ $EUID -ne 0 ]]; then
+   echo "Error:This script must be run as root!" 1>&2
+   exit 1
+fi
+
+
+Get_Dist_Name()
+{
+    if grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+        release='CentOS'
+    elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+        release='Debian'
+    elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
+        release='Ubuntu'
+	else
+        release='unknow'
+    fi
+    
+}
+Get_Dist_Name
+function getversion(){
+    if [[ -s /etc/redhat-release ]];then
+        grep -oE  "[0-9.]+" /etc/redhat-release
+    else    
+        grep -oE  "[0-9.]+" /etc/issue
+    fi    
+}
+ver=""
+CentOSversion() {
+    if [ "${release}" == "CentOS" ]; then
+        local version="$(getversion)"
+        local main_ver=${version%%.*}
+		ver=$main_ver
+    else
+        ver="$(getversion)"
+    fi
+}
+CentOSversion
+Get_OS_Bit()
+{
+    if [[ `getconf WORD_BIT` = '32' && `getconf LONG_BIT` = '64' ]] ; then
+        bit='x64'
+    else
+        bit='x32'
+    fi
+}
+Get_OS_Bit
+
+if [ "${release}" == "CentOS" ]; then
+	yum install -y bc
+else
+	apt-get update
+	apt-get install -y bc
+fi
+
+iddver=`ldd --version | grep ldd | awk '{print $NF}'`
+dver=$(echo "$iddver < 2.14" | bc)
+if [ $dver -eq 1 ]; then
+	ldd --version
+	echo "idd的版本低于2.14，系统不支持。请尝试Centos7，Debian8，Ubuntu16"
+	exit 1
+fi
+
+if [ "$bit" -ne "x64" ]; then
+	echo "脚本目前只支持64bit系统"
+	exit 1
+fi	
 
 apt-get update
+apt-get install -y wget
 wget --no-check-certificate https://raw.githubusercontent.com/mishaelre/ovz-bbr-powered/master/rinetd
 chmod +x rinetd
 
