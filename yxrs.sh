dc3e56e1c083727ee9aa14f8c6c7f00c7120dd4b
mishaelre <664513537@qq.com>
2017-06-30 20:08:06 +0800

    Update yxrs.sh

--- yxrs.sh
+++ yxrs.sh
@@ -1,10 +1,19 @@
+#! /bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+#=================================================================#
+#   System Required:  Debian8_x64                                   #
+#   Description: One click Install lkl-bbr           #
+#=================================================================#
+
+
 apt-get update
 wget --no-check-certificate https://raw.githubusercontent.com/mishaelre/ovz-bbr-powered/master/rinetd
 chmod +x rinetd
 
 cat > /root/rinetd.conf<<-EOF
 # bindadress bindport connectaddress connectport
-0.0.0.0 12420 0.0.0.0 12420
+0.0.0.0 1001 0.0.0.0 1001
 EOF
 
 cat > /etc/systemd/system/rinetd.service<<-EOF
