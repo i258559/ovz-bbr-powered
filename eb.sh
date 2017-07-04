a1ed001b70ba07b270c6be12f1a53c3ec525d8fe
mishaelre <664513537@qq.com>
2017-07-04 22:30:32 +0800

    Update eb.sh

--- eb.sh
+++ eb.sh
@@ -1,9 +1,9 @@
-cat /root/ebesucher.sh >>-EOF
+cat > /root/ebesucher.sh<<EOF
 export DISPLAY=localhost:1.0
 rm -rf ~/.vnc/*.log /tmp/plugtmp* > /dev/null
 killall /usr/bin/x-www-browser >> /dev/null 2>&1
 /usr/bin/iceweasel -new-tab http://www.ebesucher.com/surfbar/mishaelre > /dev/null 2>&1 &
-
+EOF
 chmod +x /root/ebesucher.sh
 echo "0 */2 * * * root /root/ebesucher.sh" >>/etc/crontab
 service cron restart
