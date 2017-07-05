73a0642daf5c6095b685e51a223e3c74ffbab2dc
mishaelre <664513537@qq.com>
2017-07-05 10:15:35 +0800

    Update v.sh

--- v.sh
+++ v.sh
@@ -24,48 +24,12 @@ xfce4-session &
 EOF
 chmod +x /root/.vnc/xstartup
 #设置开机启动
-touch /etc/init.d/tightvncserver
-chmod +x /etc/init.d/tightvncserver
-
-cat > /etc/init.d/tightvncserver<<-EOF
-
-#!/bin/sh
-### BEGIN INIT INFO
-# Provides: tightvncserver
-# Required-Start: $syslog $remote_fs $network
-# Required-Stop: $syslog $remote_fs $network
-# Default-Start: 2 3 4 5
-# Default-Stop: 0 1 6
-# Short-Description: Starts VNC Server on system start.
-# Description: Starts tight VNC Server. Script written by James Swineson.
-### END INIT INFO
-# /etc/init.d/tightvncserver
-VNCUSER='root'
-case "$1" in
-start)
-su $VNCUSER -c '/usr/bin/tightvncserver -geometry 800x600 -depth 24 :1'
-echo "Starting TightVNC Server for $VNCUSER"
-;;
-stop)
-pkill Xtightvnc
-echo "TightVNC Server stopped"
-;;
-*)
-echo "Usage: /etc/init.d/tightvncserver {start|stop}"
-exit 1
-;;
-esac
-exit 0`</pre>
-
-EOF
-
+chkconfig vncserver on
 #修改权限
-chmod 755 /etc/init.d/tightvncserver
 update-rc.d tightvncserver defaults
-
 #安装火狐
 apt-get install -y firefox-esr
 apt-get install -y flashplugin-nonfree
 #重启vnc
-vncserver :1
+tightvncserver :1
 apt-get install -y cron
